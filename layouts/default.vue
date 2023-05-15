db2a33f452b11a355d8f529d1b0873338b1beec1
ferry0601 <wahyuferryansyah0601@gmail.com>
2023-05-15 15:06:46 +0700

    first

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <TheHeader />
+      <Nuxt />
+      <TheFooter />
+    </div>
+</template>
+  
+
+<script>
+
+    import TheHeader from '~/components/inc/TheHeader.vue';
+    import TheFooter from '~/components/inc/TheFooter.vue';
+
+    export default{
+        name: 'default',
+        components:{
+            TheHeader,
+            TheFooter,
+
+        }
+    }
+
+</script>
